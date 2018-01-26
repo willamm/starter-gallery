e4bccf506b5919bed0c64dc7501d7d7db7ada343
William Murphy <will.murphy1994@gmail.com>
2018-01-25 17:43:56 -0800

    Refactor WelcomeController to be more MVC

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,8 +19,27 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
-		// $this->load->view('welcome');
+		// Get the newest images from the model
+		$pix = $this->images->newest();
+
+		foreach ($pix as $picture) {
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+		}
+
+		$this->load->library('table');
+		$parms = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+		$this->table->set_template($parms);
+
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+
 		$this->data['pagebody'] = 'welcome';
 		$this->render();
 	}
+
+
 }
