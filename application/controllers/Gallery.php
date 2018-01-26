fa4eabd9d4e9b40e6d1efce83180df8b1f5f0f32
William Murphy <will.murphy1994@gmail.com>
2018-01-25 17:36:07 -0800

    Gallery now shows newest images

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -8,7 +8,26 @@ class Gallery extends Application
 	public function index()
 	{
 		// $this->load->view('Gallery');
-		$this->data['pagebody'] = 'Gallery';
+		// $this->data['pagebody'] = 'Gallery';
+		// $this->render();
+		$pix = $this->images->all();
+
+		foreach ($pix as $picture) {
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+		}
+
+		$this->load->library('table');
+		$parms = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+		$this->table->set_template($parms);
+
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+
+		$this->data['pagebody'] = 'gallery';
 		$this->render();
 	}
 }
--- application/models/Images.php
+++ application/models/Images.php
@@ -11,4 +11,11 @@ class Images extends CI_Model {
         $query = $this->db->get('images');
         return $query->result_array();
     }
+
+    function newest() {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
 }
