07544d3f9fe33da00d477caee755e799d1ca2cae
William Murphy <will.murphy1994@gmail.com>
2018-01-25 16:18:10 -0800

    Add template for gallery view

---
+++ application/models/Images.php
@@ -0,0 +1,14 @@
+<?php
+
+class Images extends CI_Model {
+
+    function __contruct() {
+        parent::__construct();
+    }
+
+    function all() {
+        $this->db->order_by("id", "desc");
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+}
---
+++ application/views/_cell.php
@@ -0,0 +1,4 @@
+<a href="./data/{filename}" data-lightbox="gallery"
+    data-title="{title} ... Posted {uploaded} by {uploader}, in category">
+    <img src="./data/thumb/{filename}" />
+</a> 
